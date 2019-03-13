5ea809629b8a915bad0069ff2488c9708a89fc37
Ben Williams <8enwilliams@gmail.com>
2019-03-13 16:51:19 +1100

    some win32

--- ext/ductwork.cpp
+++ ext/ductwork.cpp
@@ -1,11 +1,26 @@
 #include "ductwork.h"
 
+// TODO: make constants overridable
+
 #ifdef _WIN32
-  // TODO: wdoze
+  #include <windows.h>
+  const char *PATH_PREFIX = "/./pipe";
+  const DWORD OPEN_MODE = PIPE_ACCESS_DUPLEX;
+  const DWORD PIPE_MODE = PIPE_TYPE_BYTE;
+  const DWORD MAX_INSTANCES = PIPE_UNLIMITED_INSTANCES;
+  const DWORD OUT_SIZE = 512;
+  const DWORD IN_SIZE = 512;
+  const DWORD TIMEOUT = 0;
+  SECURITY_ATTRIBUTES SECURITY_ATTRS = {
+    0,
+    NULL,
+    TRUE
+  };
 #else
   #include <sys/stat.h>
   #include <sys/types.h>
   #include <string.h>
+  const mode_t MKFIFO_PERMS = 666;
 #endif
 
 using Napi::String;
@@ -18,8 +33,6 @@ using Napi::Error;
 using Napi::Value;
 using Napi::Promise;
 using namespace Ductwork;
-
-const mode_t MKFIFO_PERMS = 666; // TODO: mkfifo permissions
 
 Object Ductwork::Init(Env env, Object exports) {
   exports.Set("create", Function::New(env, Create));
@@ -43,7 +56,42 @@ String Ductwork::Create(const CallbackInfo &info) {
   // TODO: move platforms off-world
 
 #ifdef _WIN32
-  // TODO: wdoze
+  std::string actualPath(PATH_PREFIX);
+  actualPath += path;
+
+  HANDLE handle = CreateNamedPipe(
+    path.c_str(),
+    OPEN_MODE,
+    PIPE_MODE,
+    MAX_INSTANCES,
+    OUT_SIZE,
+    IN_SIZE,
+    TIMEOUT,
+    &SECURITY_ATTRS
+  );
+
+  if (handle == INVALID_HANDLE_VALUE) {
+    DWORD error = GetLastError();
+    LPTSTR errorText = NULL;
+
+    FormatMessage(
+      FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,  
+      NULL,
+      error,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      &errorText,
+      0,
+      NULL
+    );
+
+    LocalFree(errorText);
+
+    std::string message("Couldn't CreateNamedPipeA: ");
+    message += std::to_string(error);
+    Error::New(env, message).ThrowAsJavaScriptException();
+  }
+
+  return String::New(env, actualPath);
 #else
   int result = mkfifo(path.c_str(), MKFIFO_PERMS);
 
@@ -60,6 +108,7 @@ String Ductwork::Create(const CallbackInfo &info) {
 
 Value Ductwork::Wait(const CallbackInfo &info) {
   Promise::Deferred deferred = Promise::Deferred::New(info.Env());
-  // TODO: wait some
+  // TODO: select()
+  // TODO: win32 WaitNamedPipe
   return deferred.Promise();
 }
